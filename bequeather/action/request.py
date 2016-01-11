43aff5ef37d77074166173e3dcec896cb7d1702a
Mechzeit <dev@mnm.id.au>
2016-01-12 01:35:55 +1100

    Added in a 'header' for RequestFileStream action

--- bequeather/action/request.py
+++ bequeather/action/request.py
@@ -2,7 +2,7 @@
 #Request HTTP/HTTPS
 from .base import BaseAction
 from bequeather.settings import get as getSettings
-import logging
+import logging, json, os
 
 logging.basicConfig(level = logging.INFO)
 logger = logging.getLogger(__name__)
@@ -15,11 +15,14 @@ class RequestFileStream(BaseAction):
 	def execute(self):
 		bufferSize = getSettings().get('chunkSize')
 		targetFile = self.parseTargetPath()
+		
 		try:
 			f = open(targetFile, 'rb')
 		except FileNotFoundError:
 			logger.warning('[!] FILE NOT FOUND')
 			return False
+
+		self.getConnection()().send(bytes(json.dumps({"routine": self.__class__.__name__, "function": self.getMethodName()(), "type": "file stream", "bytes": os.path.getsize(targetFile)}), 'ascii'))
 
 		part = f.read(bufferSize)
 		totalBytes = 0
@@ -38,9 +41,6 @@ class RequestFileStream(BaseAction):
 
 		#Flush and close the file stream
 		f.close()
-
-		#Return the file handler
-		f
 
 		self.setResponse(file = targetFile, bytes = totalBytes)
 
